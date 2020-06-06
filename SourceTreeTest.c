3ab1a89cd129874fb606faab5d08e7c47ff05dc3
rosua328 <rosua328@gmail.com>
2020-06-06 11:14:09 +0900

    휴ㅠㅠ

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -7,7 +7,7 @@ int main()
   printf("First OSS Code\n");
   printf("Second OSS Code\n");
   printf("마스터 브렌치에 코드를 추가하였음");
-  printf("dddddvgggg\n", );
+  printf("dddddvggggbbbbbb\n", );
 
 for(int i = 0; i<10; i++){
   printf("Test 브랜치\n");
